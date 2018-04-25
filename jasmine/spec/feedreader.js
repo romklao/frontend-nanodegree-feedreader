5677873a3035dea8a128b1282e3abb32d10f8aea
Romklao <rk120879@gmail.com>
2018-04-24 17:15:57 -0700

    Initial commit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,47 +26,93 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has a URL defined', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('has a name defined', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        let body = $('body'),
+              menuIcon = $('.menu-icon-link');
+
+        it('is hidden by default', function() {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility when the menu icon is clicked', function() {
+            menuIcon.click();
+            expect(body.hasClass('menu-hidden')).toBe(false);
 
+            menuIcon.click();
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        let container = $('.feed');
 
+        beforeEach(function(done) {
+            loadFeed(0, () => done());
+        });
+
+        it('should have at least a single .entry element within the .feed container', function() {
+            expect(container.has('.entry').length).not.toBe(0);
+        });
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let container = $('.feed'),
+            initialFeed;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                initialFeed = container.html();
+                // Show the content at index 2
+                loadFeed(2, () => done());
+            });
+        });
+        // Compare original feed and updated feed, they should be difference
+        it('should change the content when a new feed is loaded', function() {
+            expect(container.html()).not.toMatch(initialFeed);
+        });
+    });
 }());
--- js/app.js
+++ js/app.js
@@ -11,13 +11,16 @@ var allFeeds = [
     {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
-    }, {
+    },
+    {
         name: 'CSS Tricks',
         url: 'http://feeds.feedburner.com/CssTricks'
-    }, {
+    },
+    {
         name: 'HTML5 Rocks',
         url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
+    },
+    {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
     }
